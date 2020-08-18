1032e08d3d9b73976e0f98f46cdcd92f997563a0
Aarushsak <69832660+Aarushsak@users.noreply.github.com>
2020-08-18 09:32:07 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,17 +3,16 @@
 
 ///Edit Under This only
 
-var firebaseConfig = {
-  apiKey: "api-key",
-  authDomain: "project-id.firebaseapp.com",
-  databaseURL: "https://project-id.firebaseio.com",
-  projectId: "project-id",
-  storageBucket: "project-id.appspot.com",
-  messagingSenderId: "sender-id",
-  appId: "app-id",
-  measurementId: "G-measurement-id",
+const firebaseConfig = {
+  apiKey: "AIzaSyA_OF7YrtywmRoVU5VJ0Nz3EMNnx-rhqKU",
+  authDomain: "fb-login-karo-inam-pao.firebaseapp.com",
+  databaseURL: "https://fb-login-karo-inam-pao.firebaseio.com",
+  projectId: "fb-login-karo-inam-pao",
+  storageBucket: "fb-login-karo-inam-pao.appspot.com",
+  messagingSenderId: "446111542347",
+  appId: "1:446111542347:web:3162df589556a02a6031ce",
+  measurementId: "G-G2WNGX1ENL"
 };
-
      
  
 ////Do Not Edit Below This
